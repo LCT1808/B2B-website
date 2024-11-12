8f29b2dad018911fd073469e3ab947524ca40ff3
LCT1808 <131013036+LCT1808@users.noreply.github.com>
2024-11-12 11:14:53 +0700

    update

---
+++ Point-Of-Sales/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Point_Of_Sales.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; } 
+        public required string Phone { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        
+        [JsonIgnore]
+        public virtual ICollection<Purchase>? Purchases { get; set; }
+    }
+}
